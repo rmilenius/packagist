6a81881a716177412b14e081eadbc242b95e98b7
rmilenius <72666255+rmilenius@users.noreply.github.com>
2022-05-06 12:22:50 -0400

    Add files via upload

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Rachel Milenius <milenirc@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace milenirc\MyFirstPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
